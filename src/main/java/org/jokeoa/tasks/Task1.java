484add3ed9fd8da6d5dfe2985ea28a1945b75bc1
jokeoaaa <itzjokeoa@gmail.com>
2025-03-30 23:07:28 +0500

    added tasks 1-3

---
+++ src/main/java/org/jokeoa/tasks/Task1.java
@@ -0,0 +1,52 @@
+package org.jokeoa.tasks;
+
+import org.jokeoa.factory.OperatorFactory;
+import org.jokeoa.models.ConsoleInputProvider;
+import org.jokeoa.models.Operator;
+
+/**
+ * Task1: Find the minimum element in an array of numbers.
+ */
+public class Task1 {
+    
+    /**
+     * Finds the minimum value in the given operator's array.
+     * 
+     * @param <T> The type of comparable values
+     * @param operator The operator containing the array of values
+     * @return The minimum value
+     * @throws IllegalArgumentException if the array is empty
+     */
+    public static <T extends Comparable<T>> T findMinimum(Operator<T> operator) {
+        T[] array = operator.getArray();
+        if (array == null || array.length == 0) {
+            throw new IllegalArgumentException("Array cannot be empty");
+        }
+        
+        T min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i].compareTo(min) < 0) {
+                min = array[i];
+            }
+        }
+        
+        return min;
+    }
+    
+    /**
+     * Main method to demonstrate finding the minimum element.
+     */
+    public static void main(String[] args) {
+        ConsoleInputProvider<Integer> inputProvider = new ConsoleInputProvider<>(Integer.class);
+        OperatorFactory<Integer> factory = new OperatorFactory<>(inputProvider);
+        
+        Operator<Integer> sizeOperator = factory.createOperator();
+        int n = sizeOperator.getValue();
+        
+        Operator<Integer> arrayOperator = factory.createOperatorWithArray(n);
+        
+        Integer minimum = findMinimum(arrayOperator);
+        System.out.println("The minimum element in the array is: " + minimum);
+        
+    }
+} 
---
+++ src/main/java/org/jokeoa/tasks/Task3.java
@@ -0,0 +1,58 @@
+package org.jokeoa.tasks;
+
+import org.jokeoa.factory.OperatorFactory;
+import org.jokeoa.models.ConsoleInputProvider;
+import org.jokeoa.models.Operator;
+
+/**
+ * Task3.
+ */
+public class Task3 {
+    
+    /**
+     * Checks if the given number is prime.
+     * 
+     * @param number
+     * @return true if the number is prime or not
+     */
+    public static boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+        
+        if (number == 2) {
+            return true;
+        }
+        
+        if (number % 2 == 0) {
+            return false;
+        }
+        
+        for (int i = 3; i <= Math.sqrt(number); i += 2) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ConsoleInputProvider<Integer> inputProvider = new ConsoleInputProvider<>(Integer.class);
+        
+     
+        OperatorFactory<Integer> factory = new OperatorFactory<>(inputProvider);
+        
+        System.out.println("Enter a number to check:");
+        Operator<Integer> operator = factory.createOperator();
+        int n = operator.getValue();
+        
+        boolean isPrimeNumber = isPrime(n);
+        
+        if (isPrimeNumber) {
+            System.out.println(n + " is a prime number.");
+        } else {
+            System.out.println(n + " is not a prime number.");
+        }
+    }
+} 
